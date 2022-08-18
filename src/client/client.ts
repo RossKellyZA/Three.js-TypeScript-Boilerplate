cdc43b900148b5035965da8b29d119560348a1cb
Ross <rosshaydenkelly@gmail.com>
2022-08-18 08:42:14 +0200

    code cleanup
    - cleaned up client.ts. formatted closer to three.js documentation.
    - added ignores to .gitignore

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,47 +1,69 @@
 import * as THREE from 'three'
+import { Int8Attribute } from 'three';
 
-const scene = new THREE.Scene()
+//TODO
+//.gitignore
+//add XR button
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-camera.position.z = 2
+let camera: THREE.PerspectiveCamera;
+let scene: THREE.Scene;
+let renderer: THREE.WebGLRenderer;
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+let cube: THREE.Mesh;
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+init();
+animate();
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+function init() {
+    scene = new THREE.Scene()
 
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+    camera.position.z = 2
+
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: false } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.xr.enabled = true;
+    document.body.appendChild(renderer.domElement);
+
+    setupCube();
+
+    window.addEventListener('resize', onWindowResize, false);
+
+}
+
+function setupCube(){
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({
+        color: 0x00ff00,
+        wireframe: true,
+    });
+
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 }
 
 function animate() {
-    requestAnimationFrame(animate)
+    requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
 
-    render()
+    render();
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function render() {
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
 }
 
-animate()
